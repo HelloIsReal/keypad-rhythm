fc8f445f993058a7f691e100b5c3c15a33299884
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-09-20 16:41:14 +1200

    Fully refactored the code. Only one note at the start is played😭

--- game.gd
+++ game.gd
@@ -8,49 +8,73 @@ var songPlaying=true
 @export var songs:Array[Song]
 var songIndex:int = 0
 
-var bpmPerSecond: float = bpm/60
-var currentTime=0
+var currentTime: float = 0.0
 var currentNote=0
-var measure:=0.0
+
+var beatsPerSecond: float# = bpm/60
+var eightBeats: float
+
+
+
+#var measure:=0.0
 
 func _ready() -> void:
+	beatsPerSecond = 60/bpm
+	eightBeats = beatsPerSecond / 8
+	
+	
 	var noteRank=0
 	var noteOffset=0
 	for i in 3:
 		noteOffset+=1
 		for j in 3:
-			
 			noteRank += 1
+			
 			var newNote = note.instantiate()
 			get_tree().current_scene.add_child(newNote)
+			
 			newNote.global_position.x += 150 * j
 			newNote.global_position.y += 150 * noteOffset
 			newNote.global_position += spawnOffset
 			newNote.myRank = noteRank
 			#print("x:",i,", y:",j)
-	for i in songIndex:
-		songs[songIndex].notes[1].lane
-	songStart()
-	await get_tree().create_timer(0.4).timeout
-	var running=true
-	while(songPlaying):
-		if(running):
-			running=false
-			await get_tree().create_timer(bpmPerSecond/8).timeout
-			currentTime+=1
 			
-			print(currentTime)
-			print(bpmPerSecond)
-			if(currentTime % 5 == 0):
-				print("ping!")
-				$ding.play()
-			running=true
+	#for i in songIndex:
+		#songs[songIndex].notes[1].lane
+	songStart()
+	#await get_tree().create_timer(0.4).timeout
+	#var running=true
+	#while(songPlaying):
+		#if(running):
+			#running=false
+			#await get_tree().create_timer(bpmPerSecond/8).timeout
+			#currentTime+=1
+			#
+			#print(currentTime)
+			#print(bpmPerSecond)
+			#if(currentTime % 5 == 0):
+				#print("ping!")
+				#$ding.play()
+			#running=true
 			
 
 func _process(delta: float) -> void:
-	measure = $badApple.get_playback_position()# * (bpm / 60)
-	songs[songIndex].notes[1].lane
+	
+	#measure = $badApple.get_playback_position()# * (bpm / 60)
+	#songs[songIndex].notes[1].lane
 	#$badApple.get_playback_position()
+	if $badApple.playing:
+		currentTime = $badApple.get_playback_position()
+		
+		var currentBeat = currentTime / beatsPerSecond
+		var currentEight = currentTime / eightBeats
+		
+		var noteIndex = floori(currentEight)
+		
+		if(noteIndex != currentNote):
+			currentNote = noteIndex
+			spawnNote()
+		#print(currentTime)
 
 func songStart():
 	$badApple.play()
@@ -67,7 +91,8 @@ func _on_bad_apple_finished() -> void:
 	songPlaying=false
 
 func spawnNote():
-	pass
+	print("spawned!")
+	$ding.play()
 	
 
 
